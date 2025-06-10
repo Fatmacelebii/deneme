812cae82fc009350cec0a300b4690f746d8614e0
Fatma çelebi <fatmakaygisiz97@gmail.com>
2025-06-10 12:09:49 +0300

    HashMap

--- java/src/main/java/com/fatma/oop/HashMap/OgrenciNot.java
+++ java/src/main/java/com/fatma/oop/HashMap/OgrenciNot.java
@@ -1,3 +1,24 @@
 package com.fatma.oop.HashMap;
 
+import java.util.HashMap;
+import java.util.Map;
 
+public class OgrenciNot {
+    public static void main(String[] args){
+
+        //hashmap oluştur
+        HashMap<String, Integer> notlar = new HashMap<>();
+
+        //anahtar değer çiftlerini ekle
+        notlar.put("Ahmet", 90);
+        notlar.put("Ayşe", 85);
+        notlar.put("Mehmet", 75);
+
+        //for-each ile tüm öğrenci ve notlarını yaz
+        for (Map.Entry<String, Integer> entry: notlar.entrySet()){
+            String ogrenci = entry.getKey();
+            Integer notu = entry.getValue();
+            System.out.println(ogrenci + " " + notu);
+        }
+    }
+}
