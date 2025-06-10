586a58fb43b799e8034ae90e4038be6c106f0722
Fatma çelebi <fatmakaygisiz97@gmail.com>
2025-06-10 11:49:23 +0300

    Lambda

--- java/src/main/java/com/fatma/oop/LambdaExpressions/LambdaOrnek.java
+++ java/src/main/java/com/fatma/oop/LambdaExpressions/LambdaOrnek.java
@@ -1,4 +1,21 @@
 package com.fatma.oop.LambdaExpressions;
 
+import java.util.ArrayList;
+
 public class LambdaOrnek {
+    public static void main(String[] args){
+
+        //Liste oluştur
+        ArrayList<Integer> sayilar = new ArrayList<>();
+
+        //Sayıları ekle
+        sayilar.add(10);
+        sayilar.add(20);
+        sayilar.add(30);
+        sayilar.add(40);
+        sayilar.add(50);
+
+        //Lambda ile tüm sayıları yazdır
+        sayilar.forEach(sayi -> System.out.println(sayi));
+    }
 }
